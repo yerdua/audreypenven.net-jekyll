5eedc8bbd165f2a3ad4a30e33ea6092267955621
Audrey Penven <audrey.penven@gmail.com>
2015-05-02 01:41:06 -0700

    added photography projects

--- js/photography_page.onready.js
+++ js/photography_page.onready.js
@@ -1,12 +1,10 @@
 $(function(){
-  $('.gallery a').featherlightGallery({
+  $('#gallery a').featherlightGallery({
     afterContent: function(e){
       var height = $('.featherlight-content', this.$instance).height();
-
       $('img', this.$content).attr('style', 'max-height:' + (height - 30) + 'px;');
 
       var width = $('.featherlight-content', this.$instance).width();
-
       $('.caption', this.$content).attr('style', 'width:' + width + 'px;');
     }
   });
